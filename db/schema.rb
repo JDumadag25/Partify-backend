d6f175bd460d1701f7277f01d2a5a62347d2a394
JDumadag25 <justin.dumadag@gmail.com>
2018-06-18 18:20:39 -0400

    chat feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618152959) do
+ActiveRecord::Schema.define(version: 20180618194236) do
+
+  create_table "chat_messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
